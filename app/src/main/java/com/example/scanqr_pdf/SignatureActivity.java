0e710e64b799707b841c86c2e3ba4c78f3081196
unravel123 <anomynous992@gmail.com>
2020-09-24 11:38:26 +0700

    add camera record feature

--- app/src/main/java/com/example/scanqr_pdf/SignatureActivity.java
+++ app/src/main/java/com/example/scanqr_pdf/SignatureActivity.java
@@ -2,7 +2,6 @@ package com.example.scanqr_pdf;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
@@ -13,13 +12,21 @@ import androidx.lifecycle.LifecycleOwner;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
@@ -30,7 +37,10 @@ import java.util.concurrent.Executors;
 
 import utils.PaintView;
 
-public class SignatureActivity extends AppCompatActivity {
+public class SignatureActivity extends AppCompatActivity implements SurfaceHolder.Callback{
+
+
+    private final String VIDEO_PATH_NAME = "/mnt/sdcard/VGA_30fps_512vbrate.mp4";
 
     Button btnClear, btnOk;
     ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
@@ -38,76 +48,115 @@ public class SignatureActivity extends AppCompatActivity {
     PreviewView previewView;
     ExecutorService camereExecutor;
 
+    private MediaRecorder mMediaRecorder;
+    private Camera mCamera;
+    TextView textView;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mHolder;
+    private View mToggleButton;
+    private boolean mInitSuccesful;
+    int time = 30;
+
+    CountDownTimer countDownTimer = new CountDownTimer(30000, 1000) {
+
+        public void onTick(long millisUntilFinished) {
+            textView.setText(time-- + "");
+        }
+
+        public void onFinish() {
+            finish();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signature);
 
-        paintView = findViewById(R.id.pwSign);
+        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        mHolder = mSurfaceView.getHolder();
+        mHolder.addCallback((SurfaceHolder.Callback) this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        textView = findViewById(R.id.txtTime);
 
-        previewView = findViewById(R.id.previewView);
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-
-        cameraProviderFuture.addListener(() -> {
-            try {
-                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                Preview preview = new Preview.Builder()
-                        .build();
-
-                CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
-
-                preview.setSurfaceProvider(previewView.createSurfaceProvider());
-
-
-                cameraProvider.unbindAll();
-
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview);
-
-            } catch (ExecutionException | InterruptedException e) {
-                // No errors need to be handled for this Future.
-                // This should never be reached.
-                e.printStackTrace();
-            }
-        }, ContextCompat.getMainExecutor(this));
-
-
-        camereExecutor = Executors.newSingleThreadExecutor();
-
-        btnOk = findViewById(R.id.btnOK);
-        btnClear = findViewById(R.id.btnClear);
-
-        btnClear.setOnClickListener(new View.OnClickListener() {
+        mToggleButton = (ToggleButton) findViewById(R.id.toggleRecordingButton);
+        mToggleButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(SignatureActivity.this, "All clear", Toast.LENGTH_SHORT).show();
-                paintView.clear();
+            // toggle video recording
+            public void onClick(View v) {
+                if (((ToggleButton)v).isChecked()) {
+                    mMediaRecorder.start();
+                    countDownTimer.start();
+                }
             }
         });
 
+        paintView = findViewById(R.id.pwSign);
 
 
-        //customCanvas.draw(new Canvas().drawColor(Color.WHITE));
+    }
+    private void initRecorder(Surface surface) throws IOException {
+        // It is very important to unlock the camera before doing setCamera
+        // or it will results in a black preview
+        if(mCamera == null) {
+            mCamera = Camera.open();
+            mCamera.unlock();
+        }
+
+        if(mMediaRecorder == null)  mMediaRecorder = new MediaRecorder();
+        mMediaRecorder.setPreviewDisplay(surface);
+        mMediaRecorder.setCamera(mCamera);
+
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+        //       mMediaRecorder.setOutputFormat(8);
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mMediaRecorder.setVideoEncodingBitRate(512 * 1000);
+        mMediaRecorder.setVideoFrameRate(30);
+        mMediaRecorder.setVideoSize(640, 480);
+        mMediaRecorder.setOutputFile(VIDEO_PATH_NAME);
+
+        try {
+            mMediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            // This is thrown if the previous calls are not called with the
+            // proper order
+            e.printStackTrace();
+        }
+
+        mInitSuccesful = true;
     }
 
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+        try {
+            if(!mInitSuccesful)
+                initRecorder(mHolder.getSurface());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
 
-    //    private void bindPreview(ProcessCameraProvider cameraProvider) {
-//        Preview preview = new Preview.Builder()
-//                .build();
-//
-//        CameraSelector cameraSelector = new CameraSelector.Builder()
-//                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-//                .build();
-//
-//        preview.setSurfaceProvider(previewView.createSurfaceProvider());
-//
-//
-//        cameraProvider.unbindAll();
-//
-//        cameraProvider.bindToLifecycle(this, cameraSelector, preview);
-//
-//
-//
-//
-//    }
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+        shutdown();
+    }
+
+    private void shutdown() {
+        // Release MediaRecorder and especially the Camera as it's a shared
+        // object that can be used by other applications
+        mMediaRecorder.reset();
+        mMediaRecorder.release();
+        mCamera.release();
+
+        // once the objects have been released they can't be reused
+        mMediaRecorder = null;
+        mCamera = null;
+    }
+
 }
